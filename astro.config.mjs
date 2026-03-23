247cbec5e959aee96c93799bce37db78ae3d33a9
Tom Lorimer <tom@purplehippo.io>
2026-03-23 18:21:19 +0000

    updating splash and new temporary logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
 				{
 					icon: 'github',
 					label: 'GitHub',
-					href: 'https://github.com/Ellf/teachable-api-wrapper',
+					href: 'https://github.com/Ellf/optio-teachable',
 				},
 			],
 			plugins: [
